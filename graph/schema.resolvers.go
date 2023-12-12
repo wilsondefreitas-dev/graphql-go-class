91186d6dccf2275e6f4f4c46121df18b4bc5dfe0
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-12-12 19:19:00 -0300

    :sparkles: feat: Create mutation and query for category

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -13,12 +13,12 @@ import (
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
 	category, err := r.CategoryDB.Create(input.Name, input.Description)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return &model.Category{
-		ID: category.ID,
-		Name: category.Name,
+		ID:          category.ID,
+		Name:        category.Name,
 		Description: &category.Description,
 	}, nil
 }
@@ -30,7 +30,21 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	categories, err := r.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	var categoriesModel []*model.Category
+	for _, category := range categories {
+		categoriesModel = append(categoriesModel, &model.Category{
+			ID:          category.ID,
+			Name:        category.Name,
+			Description: &category.Description,
+		})
+	}
+
+	return categoriesModel, nil
+
 }
 
 // Courses is the resolver for the courses field.
